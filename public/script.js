b3cf6fc368adf1de07bdb171aa41408cfe5c4c83
Josiah Yeow <jsiahyeow@gmail.com>
2020-07-26 01:44:32 +1000

    update styling

--- public/script.js
+++ public/script.js
@@ -4,19 +4,19 @@ const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 const name = prompt('What is your name?')
-appendMessage('You joined')
+appendMessage('BOT', 'You joined')
 socket.emit('new-user', name)
 
 socket.on('user-connected', name => {
-    appendMessage(`${name} connected`)
+    appendMessage('BOT', `${name} connected`)
 })
 
 socket.on('chat-message', data => {
-    appendMessage(`${data.name}: ${data.message}`)
+    appendMessage(data.name, data.message)
 })
 
 socket.on('user-disconnected', name => {
-    appendMessage(`${name} disconnected`)
+    appendMessage('BOT', `${name} disconnected`)
 })
 
 messageForm.addEventListener('submit', e => {
@@ -24,12 +24,18 @@ messageForm.addEventListener('submit', e => {
     const message = messageInput.value
     socket.emit('send-chat-message', message)
     messageInput.value = ''
-    appendMessage(`You: ${message}`)
+    appendMessage('You', message)
 })
 
-function appendMessage(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
+function appendMessage(author, message) {
+    const messageDiv = document.createElement('div')
+    const nameSpan = document.createElement('span')
+    nameSpan.className = 'name'
+    const messageSpan = document.createElement('span')
+    nameSpan.innerText = `${author}: `
+    messageSpan.innerText = message
+    messageDiv.appendChild(nameSpan)
+    messageDiv.appendChild(messageSpan)
+    messageContainer.append(messageDiv)
     messageContainer.scrollTop = messageContainer.scrollHeight
 }
